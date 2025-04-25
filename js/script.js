9e7ba3c07623f7d12f4c8a39fd066c2611f4f283
Hifzatoufiq <hifzatoufiq@example.com>
2025-04-25 12:33:40 +0500

    First Class Code

---
+++ js/script.js
@@ -0,0 +1,2 @@
+// console.log("Ahad Bhai, aqal sy kaam lo");
+console.log(54554454);
---
+++ js/variablesConcept.js
@@ -0,0 +1,38 @@
+// console.log("Variables in JS");
+
+/*
+-- Variables:
+Variables are like a container, we can store data of multiple types in it.
+
+-- 3 keywords to create variables:
+var
+let 
+const
+
+
+-- 2 concepts to understand variables
+declaration: to give a name to the variable.
+initialization: to store data in a variable.
+*/
+
+// Declaration
+// var fruits;
+
+// Initialization
+// fruits = ["Apple", "Mangoes", "Grapes"];
+
+
+// var  --- Initialization+Declaration
+// var fruit = "Apple";
+// var fruit = "Banana";
+
+// let  --- Initialization
+// let fruit = "Apple";
+// fruit = "Banana";
+
+// const
+const fruit = "Grapes";
+fruit = "Apple";
+
+
+console.log(fruit);
