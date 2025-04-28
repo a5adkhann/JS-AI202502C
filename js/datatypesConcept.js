4f0f3fdf08fe142a07d2191651e8cc2f7d903f47
Hifzatoufiq <hifzatoufiq@example.com>
2025-04-28 12:44:32 +0500

    Lecture 2

---
+++ js/datatypesConcept.js
@@ -0,0 +1,28 @@
+// console.log("Datatypes Concept in JS");
+
+/*
+String -- ""
+Integer -- 123
+Boolean -- true/false
+Array -- A collection of multiple values in a single variable.
+Object -- A collection of property value pairs.
+function -- To make the code reusable & well-structured.
+*/
+
+// var name = "Ali";
+// var age = 20;
+// let isStudent = true;
+// let courses = ["JS", "Python", "PHP"];
+// let employee = {
+//     employee_name : "Ali",
+//     employee_age : 22,
+//     employee_salary : 45000
+// }
+// var checkType = typeof(employee);
+// console.log(checkType);
+
+function aptech(){
+    console.log("This is a Function");
+}
+
+aptech();
---
+++ js/operatorsConcept.js
@@ -0,0 +1,33 @@
+// console.log("Operators in JS");
+
+/*
+Operand -- Data on which operation is performed.
+Operator -- Type of operation.
+Expression -- Combination of operand and operator.
+*/
+
+// let a = 2;
+// let b = 3;
+// let c = a * b;
+// console.log(c);
+
+
+function addition(a, b){
+    console.log(a + b);
+}
+
+function subtraction(a, b){
+    console.log(a - b)
+}
+
+function division (a, b){
+    console.log(a / b);
+}
+
+function multiplication(a, b) {
+    console.log(a * b);
+}
+
+addition(5, 5);
+
+subtraction(5, 7);
