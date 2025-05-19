7308cc2e3613ef7439b9986d541cab8828ea562b
Hifzatoufiq <hifzatoufiq@example.com>
2025-05-19 12:47:25 +0500

    Lecture 9

---
+++ js/JSEvents.js
@@ -0,0 +1,14 @@
+// const button = document.getElementsByTagName("button")[0];
+// console.log(button);
+
+// button.addEventListener("click", function(){
+//     alert("Button is clicked");
+// })
+
+// function optionChanged() {
+//     console.log("Option is changed");
+// }
+
+function Typing() {
+    console.log("Typing in Input Field...");
+}
